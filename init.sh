53813a31b5dc3803e45df23ecef1ff7140ed5667
ftao <Filia.Tao@gmail.com>
2011-10-12 11:55:16 +0800

    update

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ git submodule add https://github.com/tpope/vim-git.git bundle/git
 git submodule add https://github.com/ervandew/supertab.git bundle/supertab
 git submodule add https://github.com/sontek/minibufexpl.vim.git bundle/minibufexpl
 git submodule add https://github.com/wincent/Command-T.git bundle/command-t
-git submodule add https://github.com/mitechie/pyflakes-pathogen.git
+git submodule add https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes-pathogen
 git submodule add https://github.com/mileszs/ack.vim.git bundle/ack
 git submodule add https://github.com/sjl/gundo.vim.git bundle/gundo
 git submodule add https://github.com/fs111/pydoc.vim.git bundle/pydoc
@@ -15,3 +15,8 @@ git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
 git submodule add https://github.com/vim-scripts/TaskList.vim.git bundle/tasklist
 git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
 git submodule add https://github.com/sontek/rope-vim.git bundle/ropevim
+
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
